33a3837f2a027513b054f28c7f2174f8a2d9457b
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-26 03:50:19 +0500

    Repository abstruction implemented

---
+++ src/Todo.Application/Abstructions/ITasksRepository.cs
@@ -0,0 +1,17 @@
+﻿using Todo.Domain.Enums;
+using Task = Todo.Domain.Entities.Task;
+
+namespace Todo.Application.Abstructions
+{
+    public interface ITasksRepository
+    {
+        ValueTask<Task> AddAsync(Task task);
+        ValueTask<Task> GetByIdAsync(int id);
+        ValueTask<IEnumerable<Task>> GetAllAsync();
+        ValueTask<IEnumerable<Task>> GetByPriorityAsync(PriorityLevel priority);
+        ValueTask<IEnumerable<Task>> GetByStateAsync(State state);
+        ValueTask<IEnumerable<Task>> GetByDeadlineAsync(DateTime deadline);
+        ValueTask<Task> UpdateAsync(Task task, int id);
+        ValueTask<Task> DeleteAsync(int id);
+    }
+}
