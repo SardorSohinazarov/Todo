50ca63f4d6670624445f7649ed5df0a7c0c2d7aa
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-26 03:08:41 +0500

    Comman classes implemeted for all entities

---
+++ src/Todo.Domain/Common/Auditable.cs
@@ -0,0 +1,8 @@
+﻿namespace Todo.Domain.Common
+{
+    public abstract class Auditable : BaseEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModifiedAt { get; set; }
+    }
+}
---
+++ src/Todo.Domain/Common/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Todo.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
