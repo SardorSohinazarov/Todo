51af52f1267f28c8b737e4d4897e467684c4561f
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-26 03:39:32 +0500

    Not Found Exception Implemented

---
+++ src/Todo.Domain/Exceptions/TaskNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo.Domain.Exceptions
+{
+    public class TaskNotFoundException : Exception
+    {
+        public TaskNotFoundException(int id)
+            : base(message: $"Task with ID {id} not found.")
+        {
+        }
+    }
+}
