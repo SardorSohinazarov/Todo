3d456a38ba8da2693f298d59275e46a394687df1
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-26 06:34:47 +0500

    TaskDTO created

---
+++ src/Todo.Application/DTOs/TaskDTO.cs
@@ -0,0 +1,14 @@
+﻿using Todo.Domain.Enums;
+
+namespace Todo.Application.DTOs
+{
+    public class TaskDTO
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public PriorityLevel Priority { get; set; }
+        public State State { get; set; }
+        public string? Notes { get; set; }
+    }
+}
