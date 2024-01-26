41ae5662fbd90b8bddbab12b22caa3ca0f39923c
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-27 04:46:38 +0500

    Models Created in UI

---
+++ src/Todo.UI/Models/Enums/PriorityLevel.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo.UI.Models.Enums
+{
+    public enum PriorityLevel
+    {
+        None = 0,
+        Low,
+        Medium,
+        High
+    }
+}
---
+++ src/Todo.UI/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using Todo.UI.Models.Enums;
+
+namespace Todo.UI.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public PriorityLevel Priority { get; set; }
+        public State State { get; set; }
+        public string? Notes { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModifiedAt { get; set; }
+    }
+}
