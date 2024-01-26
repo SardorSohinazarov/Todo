239f4a7a435c555c5cafb6cc0c9fb0fb68f9de60
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-26 05:25:35 +0500

    Application DbContext implemeted

---
+++ src/Todo.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Todo.Infrastructure.Data.Configurations;
+using Task = Todo.Domain.Entities.Task;
+
+namespace Todo.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        private readonly IConfiguration _configuration;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration)
+            : base(options)
+        {
+            _configuration = configuration;
+            Database.Migrate();
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        async ValueTask<int> IApplicationDbContext.SaveChangesAsync(CancellationToken cancellationToken = default)
+            => await base.SaveChangesAsync(cancellationToken);
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new TaskConfiguration());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("Default"));
+        }
+    }
+}
---
+++ src/Todo.Infrastructure/Data/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task = Todo.Domain.Entities.Task;
+
+namespace Todo.Infrastructure.Data
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Task> Tasks { get; set; }
+
+        ValueTask<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
