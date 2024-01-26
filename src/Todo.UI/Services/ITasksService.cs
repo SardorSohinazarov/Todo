980d73d79374eb27407d880952011b6f2f322619
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-27 04:46:59 +0500

    Api Services implemeted

---
+++ src/Todo.UI/Services/ITasksService.cs
@@ -0,0 +1,10 @@
+﻿using Task = Todo.UI.Models.Task;
+
+namespace Todo.UI.Services
+{
+    public interface ITasksService
+    {
+        public List<Task> Tasks { get; set; }
+        public ValueTask GetAllTasksAsync();
+    }
+}
